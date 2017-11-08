25882a9d0fcd01ac5781d3b1251d3ee111c38d96
hopkinsc4 <hopkinsc4@gmail.com>
2017-11-07 20:54:10 -0500

    fixed repo commit importing and formatting

--- gitwrap/index.js
+++ gitwrap/index.js
@@ -28,7 +28,75 @@ module.exports = {
             callback(returnUserRepos);
         })
     },
+    getRepoCommits: function(user, repoName, callback) {
+        var github = new GitHubApi({
+            headers: {
+                "User-Agent": `${user}`
+            }
+        })
 
+        var returnRepoCommits = [];
+
+        var commitCallback = new Promise((resolve, reject) => {
+            github.repos.getCommits({
+                owner: `${user}`,
+                author: `${user}`,
+                repo: `${repoName}`
+            }, (error, response) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(response);
+                }
+            })
+            }).then((commits) => {
+                var repoCommits = commits['data'];
+                repoCommitsArray = [];
+                repoCommits.forEach(function(rc) {
+
+                    var message = rc.commit.message;
+                    var author = rc.commit.author.name;
+                    var html_url = rc.html_url;
+                    var sha = rc.sha;
+
+                    var commitInfo = {
+                        "message": `${message}`,
+                        "author": `${author}`,
+                        "html_url": `${html_url}`,
+                        "sha": `${sha}`
+                    }
+
+                    repoCommitsArray.push(commitInfo);
+                })
+
+                callback(repoCommitsArray);
+            })
+            .catch((error) => {
+                callback(error);
+            })
+    }
+
+/*
+    repoCommits.forEach(function(rc) => {
+        ....
+    })
+
+    {
+        "commit": [
+            "message": "rc.commit['message']",
+            "author": "rc.commit.author.name",
+            "html_url": "rc.commit['html_url']",
+            "sha": "rc.commit[sha]"
+        ]
+    }
+
+
+*/
+
+
+
+
+    /*
     getRepoCommits: function(user, repoName, callback) {
         var github = new GitHubApi({
             headers: {
@@ -38,7 +106,7 @@ module.exports = {
         
         // TODO: optional authentication here depending on desired endpoints. See below in README.
 
-
+        
         var returnRepoCommits = [];
         var repoCommits = new Promise( (resolve, reject) => {
             github.repos.getCommits({
@@ -51,11 +119,13 @@ module.exports = {
                 } else {
                     resolve(res);
                 }
-            })
-        })
+            });
+        });
         repoCommits.then((commits) => {
             callback(commits);
-        })
+        });
+        
     }
-}
+    */
+};
 
